b0406784507b3fb690221d9968cf1274853c8f91
Rodrigo <rss.999999999@gmail.com>
2023-10-05 11:07:12 -0300

    first draft

--- src/br/com/rodrigo/banco/Conta.java
+++ src/br/com/rodrigo/banco/Conta.java
@@ -1,2 +1,51 @@
-package br.com.rodrigo.banco;public class Conta {
+/**
+ * Modificadores de acesso:
+ * <p>
+ * public: todos podem acessar
+ * private: somente a própria classe pode acessar
+ * protected: somente a própria classe e as classes filhas podem acessar
+ * default (package): somente as classes do mesmo pacote podem acessar
+ * static: o atributo ou método pertence à classe e não ao objeto
+ * final: o atributo ou método não pode ser alterado
+ * abstract: a classe não pode ser instanciada
+ */
+
+package br.com.rodrigo.banco;
+
+public class Conta {
+
+    protected int agencia;
+    int numero;
+    double saldo;
+
+    //construtor padrão
+    public Conta() {
+
+    }
+
+    //construtor de classe (com parâmetros
+    //alt + insert
+    public Conta(int agencia, int numero, double saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    //método (sem retorno - void)
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public void retirar(double valor) {
+        this.saldo -= valor;
+    }
+
+    public double verificarSaldo() {
+        return this.saldo;
+    }
+
+
 }
+
+
+
--- src/br/com/rodrigo/banco/Teste.java
+++ src/br/com/rodrigo/banco/Teste.java
@@ -1,2 +1,19 @@
-package br.com.rodrigo.banco;public class Teste {
+
+package br.com.rodrigo.banco;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        //Conta Corrente
+        Conta cc = new Conta(123, 456, 1000.0);
+        cc.depositar(100.0);
+        cc.retirar(50.0);
+        System.out.println("Saldo: " + cc.verificarSaldo());
+
+        //Conta Poupança
+        Conta cp = new Conta(123, 789, 1000.0);
+        cp.depositar(100.0);
+        cp.retirar(50.0);
+        System.out.println("Saldo: " + cp.verificarSaldo());
+    }
 }
